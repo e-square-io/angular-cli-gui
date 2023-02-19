6d952b4037f876e46f95cbbc6d678a755d26c1ba
Daniel Liverant <daniel.liverant@hibob.com>
2023-02-19 13:05:11 +0200

    fix(global): cr

--- apps/cli-gui/src/main.ts
+++ apps/cli-gui/src/main.ts
@@ -1,19 +1,35 @@
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { APP_INITIALIZER } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import { map } from 'rxjs';
 
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
+import { CoreService } from './app/core/core.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
-    importProvidersFrom(BrowserAnimationsModule),
+    provideAnimations(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (http: HttpClient, core: CoreService) => () =>
+        http.get<string[]>(`/api/workspace`).pipe(
+          map((projectNames) =>
+            core.update({
+              projectNames,
+              currentProjectName: projectNames?.[0],
+            })
+          )
+        ),
+      deps: [HttpClient, CoreService],
+      multi: true,
+    },
   ],
 }).catch((err) => console.error(err));

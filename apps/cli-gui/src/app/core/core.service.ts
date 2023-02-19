f5a593a01dcbd603a6f45156383335dd3e0bea37
Daniel Liverant <daniel.liverant@hibob.com>
2023-02-19 13:02:32 +0200

    feat(global): add theme

--- apps/cli-gui/src/app/core/core.service.ts
+++ apps/cli-gui/src/app/core/core.service.ts
@@ -10,7 +10,7 @@ export interface CoreState {
   providedIn: 'root',
 })
 export class CoreService {
-  private coreState: CoreState = { projectNames: [] };
+  private coreState: CoreState = { projectNames: ['test'] };
   private readonly coreStateSubject$ = new BehaviorSubject(this.coreState);
 
   readonly coreState$ = this.coreStateSubject$.asObservable();

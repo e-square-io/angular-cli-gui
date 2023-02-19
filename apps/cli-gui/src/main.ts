9fb65733888fc603322c00b3c208be124aa34867
Daniel Liverant <daniel.liverant@hibob.com>
2023-02-19 11:37:40 +0200

    feat(cli-gui): replace dom element with angular material elements

--- apps/cli-gui/src/main.ts
+++ apps/cli-gui/src/main.ts
@@ -1,33 +1,19 @@
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { APP_INITIALIZER } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
-import { map } from 'rxjs';
 
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
-import { CoreService } from './app/core/core.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (http: HttpClient, core: CoreService) => () =>
-        http.get<string[]>(`/api/workspace`).pipe(
-          map((projectNames) =>
-            core.update({
-              projectNames,
-              currentProjectName: projectNames?.[0],
-            })
-          )
-        ),
-      deps: [HttpClient, CoreService],
-      multi: true,
-    },
+    importProvidersFrom(BrowserAnimationsModule),
   ],
 }).catch((err) => console.error(err));

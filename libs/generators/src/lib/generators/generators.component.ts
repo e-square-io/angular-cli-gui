1ce09a4805cfcfd70f3b3b684f17f509b89ece11
eladzipper96 <elad.zipper96@gmail.com>
2023-02-22 10:40:38 +0200

    generator side nav skeleton

--- libs/generators/src/lib/generators/generators.component.ts
+++ libs/generators/src/lib/generators/generators.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'cli-generators',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './generators.component.html',
-  styleUrls: ['./generators.component.css'],
+  styleUrls: ['./generators.component.scss'],
 })
-export class GeneratorsComponent {}
+export class GeneratorsComponent {
+
+  menuItems = ['Component', 'Directive', 'Module', 'Pipe'];
+
+}

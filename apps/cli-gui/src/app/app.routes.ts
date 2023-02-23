c33a7704ca8497e83b1e44f497179f43c26db1e6
Daniel Liverant <daniel.liverant@hibob.com>
2023-02-23 17:01:34 +0200

    fix(global): fix routing

--- apps/cli-gui/src/app/app.routes.ts
+++ apps/cli-gui/src/app/app.routes.ts
@@ -1,33 +1,41 @@
 import { Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
-
 export const APP_ROUTES: Routes = [
   {
     path: '',
-    component: AppComponent,
     children: [
       {
-        path: '',
+        path: '**',
         redirectTo: 'generators',
-        pathMatch: 'full',
       },
       {
         path: 'generators',
-        loadComponent: () => import('@angular-cli-gui/generators').then(m => m.GeneratorsComponent),
+        loadComponent: () =>
+          import('@angular-cli-gui/generators').then(
+            (m) => m.GeneratorsComponent
+          ),
       },
       {
         path: 'configuration',
-        loadComponent: () => import('@angular-cli-gui/configuration').then(m => m.ConfigurationComponent),
+        loadComponent: () =>
+          import('@angular-cli-gui/configuration').then(
+            (m) => m.ConfigurationComponent
+          ),
       },
       {
         path: 'executors',
-        loadComponent: () => import('@angular-cli-gui/executors').then(m => m.ExecutorsComponent),
+        loadComponent: () =>
+          import('@angular-cli-gui/executors').then(
+            (m) => m.ExecutorsComponent
+          ),
       },
     ],
   },
   {
     path: 'workspace-manager',
-    loadChildren: () => import('@angular-cli-gui/workspace-manager').then(m => m.WORKSPACE_MANAGER_ROUTES),
+    loadChildren: () =>
+      import('@angular-cli-gui/workspace-manager').then(
+        (m) => m.WORKSPACE_MANAGER_ROUTES
+      ),
   },
 ];

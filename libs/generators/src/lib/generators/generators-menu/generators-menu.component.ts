46d680d35713f493d0ff1aa63c084e86204b785e
eladzipper96 <elad.zipper96@gmail.com>
2023-02-25 15:43:22 +0200

    fix: 🐛 use routerLink instead of Router service
    use in html routerlink instead of using on click and calling a function
    that will navigate

--- libs/generators/src/lib/generators/generators-menu/generators-menu.component.ts
+++ libs/generators/src/lib/generators/generators-menu/generators-menu.component.ts
@@ -1,23 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'cli-generators-menu',
   standalone: true,
-  imports: [CommonModule, MatListModule, MatSidenavModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './generators-menu.component.html',
   styleUrls: ['./generators-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GeneratorsMenuComponent {
-  public readonly router = inject(Router);
   public menuItems = ['component', 'directive', 'module', 'pipe'];
-
-  navigate(generatorName: string): void {
-    this.router.navigate(['generators', generatorName]);
-  }
 }

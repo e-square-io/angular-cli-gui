b3af04fcf8a92f8d5e075c61f76e4d05c1a75d73
omergronich <gronichomer@gmail.com>
2023-02-25 22:12:03 +0200

    refactor: 💡 resolved merge conflicts with my branch

--- libs/workspace-manager/src/index.ts
+++ libs/workspace-manager/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/features/workspace-manager/workspace-manager.component';
 export * from './lib/features/workspace-manager/workspace-manager.routes';
-export * from './lib/workspace-manager/data-access/connect-workspace.service';
+export * from './lib/data-access/connect-workspace.service';

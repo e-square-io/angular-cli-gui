c86f2d27e89156e908c3e3ede4b6399196f142dd
Daniel Liverant <daniel.liverant@hibob.com>
2023-02-26 11:37:41 +0200

    revert: changes

--- apps/cli-gui/src/main.ts
+++ apps/cli-gui/src/main.ts
@@ -1,32 +1,35 @@
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { APP_INITIALIZER } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import { map } from 'rxjs';
 
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
+import { CoreService } from './app/core/core.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
     provideAnimations(),
-    // {
-    //   provide: APP_INITIALIZER,
-    //   useFactory: (http: HttpClient, core: CoreService) => () =>
-    //     http.get<string[]>(`/api/workspace`).pipe(
-    //       map((projectNames) =>
-    //         core.update({
-    //           projectNames,
-    //           currentProjectName: projectNames?.[0],
-    //         })
-    //       )
-    //     ),
-    //   deps: [HttpClient, CoreService],
-    //   multi: true,
-    // },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (http: HttpClient, core: CoreService) => () =>
+        http.get<string[]>(`/api/workspace`).pipe(
+          map((projectNames) =>
+            core.update({
+              projectNames,
+              currentProjectName: projectNames?.[0],
+            })
+          )
+        ),
+      deps: [HttpClient, CoreService],
+      multi: true,
+    },
   ],
 }).catch((err) => console.error(err));

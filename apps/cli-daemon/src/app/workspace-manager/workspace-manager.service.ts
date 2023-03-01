7ec472b6d09a8e4ef595582b433a2c4d30d4616b
tomzo42 <tom.zohar@hibob.io>
2023-03-01 10:37:06 +0200

    perf(workspace-manager): added cache to get directory api

--- apps/cli-daemon/src/app/workspace-manager/workspace-manager.service.ts
+++ apps/cli-daemon/src/app/workspace-manager/workspace-manager.service.ts
@@ -4,6 +4,7 @@ import { NodeJsSyncHost } from '@angular-devkit/core/node';
 import { createWorkspaceHost } from '@angular-devkit/core/src/workspace';
 import { readWorkspace as devKitReadWorkspace } from '@angular-devkit/core/src/workspace/core';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { isDefined } from 'class-validator';
 
 import { SessionService } from '../session/session.service';
 
@@ -12,16 +13,25 @@ import { NOT_VALID_PATH_EXCEPTION } from './entities';
 
 @Injectable()
 export class WorkspaceManagerService {
+  private readonly pathDirectoriesCache = new Map<string, DirectoryDto[]>();
+
   constructor(private sessionService: SessionService) {}
 
   public async getDirectoriesInPath(
     path: string = this.sessionService.cwd
   ): Promise<DirectoryDto[]> {
+    const cachedPathDirectories: DirectoryDto[] | undefined =
+      this.pathDirectoriesCache.get(path);
+
+    if (isDefined(cachedPathDirectories)) {
+      return cachedPathDirectories as DirectoryDto[];
+    }
+
     try {
       const filesAndDirectories = await fs.readdir(path, {
         withFileTypes: true,
       });
-      return Promise.all(
+      const directories = await Promise.all(
         filesAndDirectories
           .filter((fileOrFolder) => fileOrFolder.isDirectory())
           .map(
@@ -32,6 +42,8 @@ export class WorkspaceManagerService {
               )
           )
       );
+      this.pathDirectoriesCache.set(path, directories);
+      return directories;
     } catch (e) {
       throw new BadRequestException(`${NOT_VALID_PATH_EXCEPTION}: ${path}`);
     }

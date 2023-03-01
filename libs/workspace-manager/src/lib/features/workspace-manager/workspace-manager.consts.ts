92f23b4496f5d29c4a321ac623ac650298918423
Omer Gronich <59396491+OmerGronich@users.noreply.github.com>
2023-03-01 16:32:24 +0200

    Update libs/workspace-manager/src/lib/features/workspace-manager/workspace-manager.consts.ts
    Co-authored-by: Daniel <liverant95@gmail.com>

--- libs/workspace-manager/src/lib/features/workspace-manager/workspace-manager.consts.ts
+++ libs/workspace-manager/src/lib/features/workspace-manager/workspace-manager.consts.ts
@@ -3,6 +3,6 @@ export const LINKS = [
   { displayName: 'Workspaces', href: 'workspaces-list', icon: 'view_list' },
   { displayName: 'Create', href: 'create-workspace', icon: 'add_circle' },
   { displayName: 'Connect', href: 'connect-workspace', icon: 'link' },
-] as const satisfies Readonly<Array<BaseLink>>;
+] as const satisfies Readonly<BaseLink[]>;
 
 export type Link = (typeof LINKS)[number];

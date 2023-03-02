b990b39dd4383a64f0fd501478a18c5e805ed697
idan.yadgar <yadgaridan@gmail.com>
2023-03-02 10:47:58 +0200

    feat: 🎸 generators

--- apps/cli-daemon/src/app/generators/generators.service.ts
+++ apps/cli-daemon/src/app/generators/generators.service.ts
@@ -58,10 +58,7 @@ export class GeneratorsService {
   private getArgsFromParams(params: Record<string, any>): string[] {
     return Object.entries(params).map(([key, value]) => {
       const convertedKey = convertKeyToArgument(key);
-      if (typeof value === 'string' || typeof value === 'number') {
-        return `${convertedKey}=${value}`;
-      }
-      return `${convertedKey}`;
+      return `${convertedKey}=${value}`;
     });
   }
 }

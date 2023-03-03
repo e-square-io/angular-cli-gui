82fba4ab92b0a4d440132d511ce6b290145c2efe
Daniel Liverant <daniel.liverant@hibob.com>
2023-03-03 11:24:19 +0200

    feat: create and use cli-menu

--- libs/ui/src/lib/components/menu/menu.component.spec.ts
+++ libs/ui/src/lib/components/menu/menu.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { GeneratorsMenuComponent } from './generators-menu.component';
+import { MenuComponent } from './menu.component';
 
 describe('GeneratorsMenuComponent', () => {
-  let component: GeneratorsMenuComponent;
-  let fixture: ComponentFixture<GeneratorsMenuComponent>;
+  let component: MenuComponent;
+  let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GeneratorsMenuComponent, RouterTestingModule],
+      imports: [MenuComponent, RouterTestingModule],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(GeneratorsMenuComponent);
+    fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

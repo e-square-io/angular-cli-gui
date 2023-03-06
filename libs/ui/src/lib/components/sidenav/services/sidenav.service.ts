c3532f1256ee1d4cad51c675d73d44409c8d8888
Daniel Liverant <daniel.liverant@hibob.com>
2023-03-06 13:32:11 +0200

    fix: cr

--- libs/ui/src/lib/components/sidenav/services/sidenav.service.ts
+++ libs/ui/src/lib/components/sidenav/services/sidenav.service.ts
@@ -3,19 +3,13 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class SidenavService {
-  private _isExpanded = true;
-  private isExpanded = new BehaviorSubject<boolean>(this._isExpanded);
+  private isExpanded = new BehaviorSubject<boolean>(true);
 
   get isExpanded$(): Observable<boolean> {
     return this.isExpanded.asObservable();
   }
 
   toggle(): void {
-    this.update(!this._isExpanded);
-  }
-
-  private update(state: boolean): void {
-    this._isExpanded = state;
-    this.isExpanded.next(this._isExpanded);
+    this.isExpanded.next(!this.isExpanded.value);
   }
 }
